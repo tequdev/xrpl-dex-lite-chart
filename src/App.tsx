1e047f96426489f3383158aeed6785c4c5a6712d
tequ <git@tequ.dev>
2024-04-19 18:57:43 +0900

    Update to allow switching of charts

--- src/App.tsx
+++ src/App.tsx
@@ -1,104 +1,147 @@
-import { useEffect, useRef } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
-import { createChart, CrosshairMode, IChartApi } from 'lightweight-charts';
+import { createChart, CrosshairMode, UTCTimestamp } from 'lightweight-charts';
 import { StackedBarsSeries } from './plugins/stacked-bars-series/stacked-bars-series'
 
+type ChartType = 'AMM' | 'CLOB' | 'DEVIATION'
+
+type MarketData = {
+  timestamp: string;
+  open: number;
+  high: number;
+  low: number;
+  close: number;
+  base_volume: number;
+  counter_volume: number;
+  exchanges: number
+}
+
 function App() {
-  const candleChartRef = useRef(null);
-  const barChartRef = useRef(null);
-  
+  const chartRef = useRef(null);
+  const [chartType, setChartType] = useState<ChartType>('CLOB')
+  const [ammData, setAmmData] = useState<MarketData[]>([])
+  const [clobData, setClobData] = useState<MarketData[]>([])
+
   useEffect(() => {
-    let candleChart: IChartApi;
-    let barChart: IChartApi;
-    [candleChartRef, barChartRef].forEach(chartRef => {
-      if (chartRef.current) {
-        (chartRef.current as any).innerHTML = '';
-        const chart = createChart(chartRef.current, {
-          width: 800,
-          height: 600,
-          layout: {
-            background: {
-              color: '#ffffff',
-            },
-            textColor: 'rgba(33, 56, 77, 1)',
-          },
-          crosshair: {
-            mode: CrosshairMode.Normal,
-          },
-          grid: {
-            vertLines: {
-              color: 'rgba(197, 203, 206, 0.5)',
-            },
-            horzLines: {
-              color: 'rgba(197, 203, 206, 0.5)',
-            },
-          },
-        });
-        if (chartRef === candleChartRef) {
-          candleChart = chart;
-        } else if (chartRef === barChartRef) {
-          barChart = chart;
+    const fetchChartData = async () => {
+      const base = 'XRP'
+      const counter = 'rcEGREd8NmkKRE8GE424sksyt1tJVFZwu_5553444300000000000000000000000000000000'
+      // const base = 'rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B_USD'
+      // const counter = 'rhub8VRN55s94qWKDv6jmDy1pUykJzF3wq_USD'
+
+      const responseAMM = await fetch(`https://data.xrplf.org/v1/iou/market_data/${base}/${counter}?interval=8h&limit=321&descending=true&only_amm=true`);
+      const responseCLOB = await fetch(`https://data.xrplf.org/v1/iou/market_data/${base}/${counter}?interval=8h&limit=321&descending=true&exclude_amm=true`);
+
+      const ammjson = (await responseAMM.json()).reverse();
+      const clobjson = (await responseCLOB.json()).reverse();
+      setAmmData(ammjson)
+      setClobData(clobjson)
+    }
+    fetchChartData()
+  }, [])
+
+  useEffect(() => {
+    if(!ammData.length || !clobData.length) return
+    if (!chartRef.current) return
+    (chartRef.current as any).innerHTML = '';
+    const chart = createChart(chartRef.current, {
+      width: 800,
+      height: 600,
+      rightPriceScale: {
+        visible: true,
+      },
+      leftPriceScale: {
+        visible: true,
+        borderVisible: false,
+      },
+      layout: {
+        background: {
+          color: '#ffffff',
+        },
+        textColor: 'rgba(33, 56, 77, 1)',
+      },
+      crosshair: {
+        mode: CrosshairMode.Normal,
+      },
+      grid: {
+        vertLines: {
+          color: 'rgba(197, 203, 206, 0.5)',
+        },
+        horzLines: {
+          color: 'rgba(197, 203, 206, 0.5)',
+        },
+      },
+    });
+
+
+    const candlestickSeries = chart.addCandlestickSeries({
+      priceScaleId: 'right',
+    });
+    const stackedbarSeries = chart.addCustomSeries(new StackedBarsSeries(), {
+      priceScaleId: 'left',
+      autoscaleInfoProvider: (original: any) => {
+        const res = original();
+        if (res !== null) {
+          res.priceRange.minValue = 0;
+          res.priceRange.maxValue *= 5;
         }
+        return res;
       }
     })
 
-      const candlestickSeries = candleChart.addCandlestickSeries();
-      const stackedbarSeries = barChart.addCustomSeries(new StackedBarsSeries())
-
-      // APIからデータを取得
-      const fetchData = async () => {
-        try {
-          const base = 'XRP'
-          const counter = 'rcEGREd8NmkKRE8GE424sksyt1tJVFZwu_5553444300000000000000000000000000000000'
-          // const base = 'rvYAfWj5gh67oV6fW32ZzP3Aw4Eubs59B_USD'
-          // const counter = 'rhub8VRN55s94qWKDv6jmDy1pUykJzF3wq_USD'
-          
-          const responseAMM = await fetch(`https://data.xrplf.org/v1/iou/market_data/${base}/${counter}?interval=8h&limit=321&descending=true&only_amm=true`);
-          const responseCLOB = await fetch(`https://data.xrplf.org/v1/iou/market_data/${base}/${counter}?interval=8h&limit=321&descending=true&exclude_amm=true`);
-          
-          const ammjson = (await responseAMM.json()).reverse();
-          const clobjson = (await responseCLOB.json()).reverse();
-          {
-            const data = ammjson.map(item => {
-              console.log(item);
-              const timestamp = new Date(item.timestamp).getTime() / 1000; // UNIXタイムスタンプに変換
-              const clob = clobjson.find(d => d.timestamp === item.timestamp);
-              return {
-                time: timestamp,
-                open: parseFloat(item.open) / parseFloat(clob.open),
-                high: parseFloat(item.high) / parseFloat(clob.high),
-                low: parseFloat(item.low) / parseFloat(clob.low),
-                close: parseFloat(item.close) / parseFloat(clob.close),
-              };
-            });
-            candlestickSeries.setData(data);
-          }
-          
-          {
-            const data = ammjson.map(item => {
-              console.log(item);
-              const timestamp = new Date(item.timestamp).getTime() / 1000; // UNIXタイムスタンプに変換
-              const clob = clobjson.find(d => d.timestamp === item.timestamp);
-              return {
-                time: timestamp,
-                values: [parseFloat(item.base_volume), parseFloat(clob.base_volume)],
-              };
-            });
-            stackedbarSeries.setData(data);
-          }
-        } catch (error) {
-          console.error("Error fetching data:", error);
-        }
-      };
-      fetchData()
+    const getValueByType = (amm: number, clob: number) => {
+      if (chartType === 'AMM') {
+        return amm;
+      }
+      if (chartType === 'CLOB') {
+        return clob;
+      }
+      if (chartType === 'DEVIATION') {
+        return amm / clob;
+      }
+      throw new Error('Invalid chart type');
     }
-  , [])
+
+    {
+      const data = ammData.map(item => {
+        console.log(item);
+        const timestamp = new Date(item.timestamp).getTime() / 1000; // UNIXタイムスタンプに変換
+        const clob = clobData.find(d => d.timestamp === item.timestamp)!;
+        return {
+          time: timestamp as UTCTimestamp,
+          open: getValueByType(item.open, clob.open),
+          high: getValueByType(item.high, clob.high),
+          low: getValueByType(item.low, clob.low),
+          close: getValueByType(item.close, clob.close),
+        };
+      });
+      candlestickSeries.setData(data);
+    }
+
+    {
+      const data = ammData.map(item => {
+        console.log(item);
+        const timestamp = new Date(item.timestamp).getTime() / 1000; // UNIXタイムスタンプに変換
+        const clob = clobData.find(d => d.timestamp === item.timestamp)!;
+        return {
+          time: timestamp as UTCTimestamp,
+          values: [item.base_volume, clob.base_volume],
+        };
+      });
+      stackedbarSeries.setData(data);
+    }
+  }, [ammData, chartType, clobData])
 
   return (
     <>
-      <h1 style={{ color: 'red' }}>AMM price deviation from CLOB</h1>
-      <div ref={candleChartRef} style={{ width: '800px', height: '600px', marginBottom: '20px' }}/>
-      <div ref={barChartRef} style={{ width: '800px', height: '600px' }}/>
+      <h1 style={{ color: 'red' }}>XRP AMM/CLOB Chart</h1>
+      <h2>{chartType}</h2>
+      <select value={chartType} onChange={(e) => setChartType(e.target.value as ChartType)}>
+        <option value="AMM">AMM</option>
+        <option value="CLOB">CLOB</option>
+        <option value="DEVIATION">DEVIATION</option>
+      </select>
+      <div ref={chartRef} style={{ width: '800px', height: '600px', marginBottom: '20px' }} />
     </>
   )
 }
